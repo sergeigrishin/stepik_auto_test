b0825e7d149154267f70828dfa0298855deae444
Sergei <sergeigrishin.go@gmail.com>
2026-03-27 15:26:19 +0300

    new file student

---
+++ student.py
@@ -0,0 +1,29 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from math import *
+
+browser = webdriver.Chrome()
+browser.get("https://suninjuly.github.io/explicit_wait2.html")
+
+try:
+    WebDriverWait(browser, 12).until(
+        EC.text_to_be_present_in_element((By.ID, "price"), "100"))
+    browser.find_element(By.ID, "book").click()
+
+    x_element = browser.find_element(By.ID, "input_value")
+    x_value = x_element.text  # Здесь должно быть только число, например "24"
+
+    result = str(log(abs(12 * sin(int(x_value)))))
+
+    input_field = browser.find_element(By.ID, "answer")
+    input_field.send_keys(result)
+
+    browser.find_element(By.ID, "solve").click()
+    time.sleep(10)
+
+finally:
+    browser.quit()
+

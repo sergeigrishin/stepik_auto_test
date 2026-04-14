b5b01faee06f7eb5a505704b3654c6b34c320c40
Sergei <sergeigrishin.go@gmail.com>
2026-04-14 14:40:46 +0300

    new fix 14/04/2026

--- QA-Prod.py
+++ QA-Prod.py
@@ -1,16 +1,33 @@
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
-browser = webdriver.Chrome()
-link  = "https://the-internet.herokuapp.com/status_codes"
-browser.get(link)
+# 1. Настройка опций
+options = webdriver.ChromeOptions()
+options.add_argument("--window-size=1280,800")
 
-my_elements = browser.find_elements("xpath", "//li/a")
+# 2. Правильная инициализация (ОДИН РАЗ)
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=options)
 
+link = "https://the-internet.herokuapp.com/status_codes"
 
-for elem in my_elements:
-    elem.click()
-    sleep(2)
-    browser.back()
-    sleep(2)
+try:
+    # 3. Используем переменную 'driver' вместо 'webdriver'
+    driver.get(link)
+    # Используем By.XPATH для надежности
+    my_elements = driver.find_elements(By.XPATH, "//li/a")
+
+    # Создаем список ссылок, так как после клика и возврата элементы могут "протухнуть" (StaleElementReferenceException)
+    hrefs = [elem.get_attribute("href") for elem in my_elements]
+
+    for href in hrefs:
+        driver.get(href)
+        sleep(1) # Пауза, чтобы рассмотреть страницу
+        driver.back()
+
+finally:
+    # 4. Закрываем браузер
+    driver.quit()
